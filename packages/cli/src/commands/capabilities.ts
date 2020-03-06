c2577dc9185dbafec2485c0a6e3e9f0c0e3fa57e
Jake Stark <jake.stark@smartthings.com>
2020-03-06 11:51:30 -0600

    PI-853 create a custom capability commands in CLI

---
+++ packages/cli/src/commands/capabilities.ts
@@ -0,0 +1,32 @@
+import { APICommand } from '@smartthings/cli-lib'
+
+
+export default class Capabilities extends APICommand {
+	static description = 'get a specific capability from a user\'s account'
+
+	static flags = APICommand.flags
+
+	static args = [
+		{
+			name: 'id',
+			description: 'the capability id',
+			required: true,
+		},
+		{
+			name: 'version',
+			description: 'the capability version',
+			required: true,
+		}
+	]
+
+	async run(): Promise<void> {
+		const { args, flags } = this.parse(Capabilities)
+		await super.setup(args, flags)
+		this.client.capabilities.get(args.id, args.version).then(async capability => {
+			this.log(JSON.stringify(capability, null, 4))
+		}).catch(err => {
+			console.log(err.response.data.error)
+			this.log(`caught error ${err}`)
+		})
+	}
+}
---
+++ packages/cli/src/commands/capabilities/update.ts
@@ -0,0 +1,62 @@
+import { flags } from '@oclif/command'
+
+import { CapabilityUpdate } from '@smartthings/core-sdk/src/endpoint/capabilities'
+
+import { APICommand } from '@smartthings/cli-lib'
+
+
+export default class CapabilitiesUpdate extends APICommand {
+	static description = 'update a capability'
+
+	static flags = {
+		...APICommand.flags,
+		data: flags.string({
+			char: 'd',
+			description: 'JSON data for capability',
+		}),
+	}
+
+	static args = [
+		{
+			name: 'id',
+			description: 'the capability id',
+			required: true
+		},
+		{
+			name: 'version',
+			description: 'the capability version',
+			required: true
+		}
+	]
+
+	private updateAndDisplay(capabilityId: string, capabilityVersion: number, capability: CapabilityUpdate): void {
+		this.client.capabilities.update(capabilityId, capabilityVersion, capability)
+			.then(updatedCapability => {
+				this.log(JSON.stringify(updatedCapability, null, 4))
+			})
+			.catch(err => {
+				this.log(`caught error ${err}`)
+			})
+	}
+
+	async run(): Promise<void> {
+		const { args, flags } = this.parse(CapabilitiesUpdate)
+		await super.setup(args, flags)
+
+		if (flags.data) {
+			const capability: CapabilityUpdate = JSON.parse(flags.data)
+			this.updateAndDisplay(args.id, args.version, capability)
+		} else {
+			const stdin = process.stdin
+			const inputChunks: string[] = []
+			stdin.resume()
+			stdin.on('data', chunk => {
+				inputChunks.push(chunk.toString())
+			})
+			stdin.on('end', () => {
+				const capability = JSON.parse(inputChunks.join())
+				this.updateAndDisplay(args.id, args.version, capability)
+			})
+		}
+	}
+}
