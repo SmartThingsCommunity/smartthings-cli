459ee203e5dcc4fb1fd6e36a1a6322697144dcde
Melody Vang <melodyvang@MN-MVANG-M.local>
2020-05-26 14:52:39 -0500

    PI-825 swapped axes and included more fields in locations/create.ts

--- packages/cli/src/commands/locations/create.ts
+++ packages/cli/src/commands/locations/create.ts
@@ -12,8 +12,17 @@ export default class LocationsCreate extends InputOutputAPICommand<LocationCreat
 
 	protected buildTableOutput(location: Location): string {
 		// TODO: swap axes, include more fields, share with ../locations.ts
-		const table = new Table({ head: ['Id', 'Name'] })
-		table.push([location.locationId, location.name])
+		const table = new Table()
+		table.push(['Name', location.name])
+		table.push(['Id', location.locationId])
+		table.push(['Country', location.countryCode])
+		table.push(['Timezone', location.timeZoneId])
+		table.push(['Background Image', location.backgroundImage])
+		table.push(['Latitude, Longitude', location.latitude, location.longitude])
+		table.push(['Region Radius', location.regionRadius])
+		table.push(['Temperature Scale', location.temperatureScale])
+		table.push(['Locale', location.locale])
+		table.push(['Additional Properties', location.additionalProperties])
 		return table.toString()
 	}
 
