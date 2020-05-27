d3c1c1ee83dab4cd982b9ccd4f0df6f1d70e3c34
Melody Vang <melodyvang@mn-mvang-m.lan>
2020-05-27 09:43:46 -0500

    shared buildTableOutput with ../locations.ts

--- packages/cli/src/commands/locations.ts
+++ packages/cli/src/commands/locations.ts
@@ -1,5 +1,24 @@
+import Table from 'cli-table'
+
 import { APICommand } from '@smartthings/cli-lib'
 
+import { Location } from '@smartthings/core-sdk'
+
+
+export function buildTableOutput(location: Location): string {
+	const table = new Table()
+	table.push(['Name', location.name])
+	table.push(['Id', location.locationId])
+	table.push(['Country', location.countryCode])
+	table.push(['Timezone', location.timeZoneId])
+	table.push(['Background Image', location.backgroundImage])
+	table.push(['Latitude, Longitude', location.latitude, location.longitude])
+	table.push(['Region Radius', location.regionRadius])
+	table.push(['Temperature Scale', location.temperatureScale])
+	table.push(['Locale', location.locale])
+	table.push(['Additional Properties', location.additionalProperties])
+	return table.toString()
+}
 
 export default class Locations extends APICommand {
 	static description = 'get a specific Location'
@@ -11,6 +30,10 @@ export default class Locations extends APICommand {
 		description: 'the location id',
 		required: true,
 	}]
+
+	protected buildTableOutput(location: Location): string {
+		return buildTableOutput(location)
+	}
 
 	async run(): Promise<void> {
 		const { args, argv, flags } = this.parse(Locations)
