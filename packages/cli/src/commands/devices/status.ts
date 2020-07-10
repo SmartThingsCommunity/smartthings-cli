feb9a7705105d2772b235ee32a3f656f33eaba6b
Bob Florian <bob@smartthings.com>
2020-07-10 14:38:29 -0400

    Added table to devices:status

--- packages/cli/src/commands/devices/status.ts
+++ packages/cli/src/commands/devices/status.ts
@@ -1,7 +1,32 @@
 import { Device, DeviceStatus } from '@smartthings/core-sdk'
 
-import {SelectingOutputAPICommand} from '@smartthings/cli-lib'
+import {APICommand, SelectingOutputAPICommand} from '@smartthings/cli-lib'
 
+
+export function buildTableOutput(this: APICommand, data: DeviceStatus): string {
+	let output = ''
+	if (data.components) {
+		for (const componentId of Object.keys(data.components)) {
+			const table = this.tableGenerator.newOutputTable({head: ['Capability', 'Attribute','Value']})
+			output += `\n${componentId} component\n`
+			const component = data.components[componentId]
+			for (const capabilityName of Object.keys(component)) {
+				const capability = component[capabilityName]
+				for (const attributeName of Object.keys(capability)) {
+					const attribute = capability[attributeName]
+					table.push([
+						capabilityName,
+						attributeName,
+						attribute.value !== null ?
+							`${JSON.stringify(attribute.value)}${attribute.unit ? ' ' + attribute.unit : ''}` : ''])
+				}
+			}
+			output += table.toString()
+			output += '\n'
+		}
+	}
+	return output
+}
 
 export default class DeviceStatusCommand extends SelectingOutputAPICommand<DeviceStatus, Device> {
 	static description = "get the current status of all of a device's component's attributes"
@@ -12,6 +37,8 @@ export default class DeviceStatusCommand extends SelectingOutputAPICommand<Devic
 		name: 'id',
 		description: 'the device id',
 	}]
+
+	protected buildTableOutput = buildTableOutput
 
 	primaryKeyName = 'deviceId'
 	sortKeyName = 'label'
