34c6b76abfafebf4c7c8e3c71f9d2fbc4821380c
John Undersander <john.undersander@smartthings.com>
2020-10-22 10:25:12 -0500

    CI(publish): fix order of version/pkg, automate zip upload

--- publish.sh
+++ publish.sh
@@ -9,10 +9,10 @@
 # 3. npm login as smartthingspi
 # 4. local master is up to date with upstream and no changes have been committed on top
 #
-# All configuration not shown in commands here is set in lerna.json under "command.publish".
-# "command.publish.conventionalPrerelease" and "command.publish.preid" should be removed once
-# we are out of pre-release. Further one-time lerna commands will be needed to "graduate" packages out
-# of pre.
+# All configuration not shown in commands here is set in lerna.json under "command.publish" or release-it.yaml.
+# Once we are out of pre-release, if we are still using release-it to update releases with binaries, we'll need
+# to set "preRelease: false". If we are still using lerna, "command.publish.conventionalPrerelease" and
+# "command.publish.preid" should be removed. Further one-time lerna commands will be needed to "graduate" packages out of pre.
 #
 # See https://github.com/lerna/lerna/blob/f6e7a13e60fefc523d701efddfcf0ed41a77749b/commands/version/README.md#prerelease
 
@@ -24,8 +24,8 @@ export GIT_COMMITTER_NAME=smartthingspi
 export GIT_COMMITTER_EMAIL=pi-team@smartthings.com
 
 if [ -z $GH_TOKEN ]; then
-    echo "ERROR: lerna requires GH_TOKEN environment variable to be set to create Github release."
-    exit 1
+	echo "ERROR: lerna requires GH_TOKEN environment variable to be set to create Github release."
+	exit 1
 fi
 
 # verify npm auth and ping just in case to prevent commiting everything and then failing
@@ -42,25 +42,35 @@ rm -rf packages/*/dist_bin
 lerna bootstrap
 lerna run clean
 lerna run compile
+lerna run test
 
-# build binaries
+# build binaries (duplicated as a test first before versioning/publishing)
+lerna run package
+
+# ignoring lifecycle scripts for now due to an error running oclif-dev inside the lerna version and publish commands.
+# the "version" npm-scripts all work outside of those commands by running "lerna run version"
+lerna publish --ignore-scripts
+
+# repeat after lerna bumped and committed versioned files
+lerna run compile
 lerna run package
 
 function zip_binary() {
 	os=$1
 	ext=$2
-	(cd packages/cli/dist_bin
+	(
+		cd packages/cli/dist_bin
 		mv "cli-$os$ext" "smartthings$ext"
 		zip ../../../smartthings-$os.zip "smartthings$ext"
 		mv "smartthings$ext" "cli-$os$ext"
 	)
 }
 
-# zip package output binaries for uploading (manually for now) to github releases
+# zip package output binaries for uploading to github releases
 zip_binary linux
 zip_binary macos
 zip_binary win .exe
 
-# ignoring lifecycle scripts for now due to an error running oclif-dev inside the lerna version and publish commands.
-# the "version" npm-scripts all work outside of those commands by running "lerna run version"
-lerna publish --ignore-scripts
+# update github release with zipped binaries (bulk of config in .release-it.yaml)
+# required due to lerna not supporting assets https://github.com/lerna/lerna/issues/2743
+npx release-it -VV --no-increment --no-npm --ci
