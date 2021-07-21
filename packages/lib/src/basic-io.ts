5b02dbe9d2aadcf0c1f82d1e86465a5ef6681768
Ross Stenersen <rossiam@gmail.com>
2021-07-21 11:36:15 -0500

    docs: add documentation to Naming and Sorting interfaces

--- packages/lib/src/basic-io.ts
+++ packages/lib/src/basic-io.ts
@@ -18,12 +18,42 @@ export type IdTranslationFunction<ID, L> = (idOrIndex: ID | string, listFunction
 export type IdRetrievalFunction<ID, L> = (fieldInfo: Sorting, list: L[], promptMessage?: string) => Promise<ID>
 
 
+/**
+ * This interface is used when a list is presented to the user, especially when they will have the
+ * opportunity to select an item from the list.
+ *
+ * The primary (but not only) example of this the list/get versions of commands. Consider the
+ * simple `locations` command. When specified without an id or index, it needs to present the
+ * results in a consistent order. If the user specifies an index into that list when querying
+ * a single location, the sort key specified here is used again to ensure the same ordering.
+ */
 export interface Sorting {
+	/**
+	 * The primary key used to uniquely identify this object.
+	 */
 	primaryKeyName: string
+
+	/**
+	 * The field you want to sort by when presenting a list of items.
+	 */
 	sortKeyName: string
 }
+
+/**
+ * This interface is used in configurations to help describe a named item.
+ *
+ * If you're writing code that uses this interface, use `itemName` or `pluralItemName` from
+ * command-util to translate a `Naming` instance to a name.
+ */
 export interface Naming {
+	/**
+	 * The singular name of your item, using lowercase letters and spaces to separate words.
+	 */
 	itemName?: string
+
+	/**
+	 * You only need to specify the plural version of your name if adding a simple "s" is incorrect.
+	 */
 	pluralItemName?: string
 }
 
