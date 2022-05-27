9a8a9d4f2e703873c0606e2d920047c3f049c3e9
John Undersander <19363370+john-u@users.noreply.github.com>
2022-05-27 16:34:54 -0500

    ci: add github release notes step

---
+++ generate-release-notes.mjs
@@ -0,0 +1,9 @@
+import { readFile, writeFile } from 'fs/promises'
+import { getChangelogEntry } from '@changesets/release-utils'
+
+
+const changelog = await readFile('CHANGELOG.md', 'utf-8')
+const version = process.argv[2]
+
+const content = getChangelogEntry(changelog, version).content
+await writeFile('RELEASE_NOTES.txt', content)
