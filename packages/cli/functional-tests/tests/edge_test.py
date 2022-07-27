b7578b7eaf44a255347c758d30a29b83e40a7191
John Undersander <19363370+john-u@users.noreply.github.com>
2022-07-27 15:00:25 -0500

    ci: add functional tests

---
+++ packages/cli/functional-tests/tests/edge_test.py
@@ -0,0 +1,8 @@
+from pexpect.popen_spawn import PopenSpawn
+
+
+def test_edge_plugin_is_installed_and_callable():
+    process = PopenSpawn('smartthings edge --help')
+    exit_status = process.wait()
+
+    assert exit_status == 0
