f2ebdc21ee1f6b369c0479c2546f34b1f3e5b228
John Undersander <19363370+john-u@users.noreply.github.com>
2022-08-10 16:02:05 -0500

    ci: add ARM build support
    - replace bash script with Node script
    - tar.gz files instead of using zip

---
+++ packages/cli/zip-binaries.mjs
@@ -0,0 +1,43 @@
+// @ts-check
+
+import { readdir, mkdir, rename } from 'fs/promises'
+import archiver from 'archiver'
+import fs from 'fs'
+import path from 'path'
+
+
+/**
+ * This script is used to post-process the executables produced by https://github.com/vercel/pkg
+ */
+const DIST_BIN = 'dist_bin'
+
+const archives = []
+const pkgs = (await readdir(DIST_BIN)).filter(name => name.startsWith('cli'))
+for (const pkg of pkgs) {
+	const targetName = path.basename(pkg, '.exe')
+
+	// pkg names have elements separated by dashes (ex. cli-macos-x64, cli-linux-arm64)
+	// if only one arch is specified during build, it is omitted (ex. cli-macos, cli-linux)
+	const elements = targetName.split('-')
+	const [, platform, arch] = elements.length === 3 ? elements : elements.concat('x64')
+
+	const ext = platform === 'win' ? '.exe' : ''
+	const binaryName = `smartthings${ext}`
+
+	const pkgName = path.join(DIST_BIN, pkg)
+	const newDir = path.join(DIST_BIN, platform, arch)
+	const newName = path.join(newDir, binaryName)
+
+	await mkdir(newDir, { recursive: true })
+	await rename(pkgName, newName)
+
+	const archiveName = path.join(newDir, `smartthings-${platform}-${arch}.tar.gz`)
+
+	const archive = archiver('tar', { gzip: true })
+	archive.append(fs.createReadStream(newName), { name: binaryName, mode: 0o755 })
+	archive.pipe(fs.createWriteStream(archiveName))
+
+	archives.push(archive.finalize())
+}
+
+await Promise.all(archives)
