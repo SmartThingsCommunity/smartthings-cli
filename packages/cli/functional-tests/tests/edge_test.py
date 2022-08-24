6b8ca6de1d68d05a1e3a3d4a50ec5150b2e2b7a6
Ross Stenersen <rossiam@gmail.com>
2022-08-24 11:03:22 -0500

    fix: update libraries so edge plugin commands work again

--- packages/cli/functional-tests/tests/edge_test.py
+++ packages/cli/functional-tests/tests/edge_test.py
@@ -3,6 +3,10 @@ from pexpect.popen_spawn import PopenSpawn
 
 def test_edge_plugin_is_installed_and_callable():
     process = PopenSpawn('smartthings edge --help')
+
     exit_status = process.wait()
 
     assert exit_status == 0
+
+    process.expect('edge:channels')
+    process.expect('edge:drivers')
