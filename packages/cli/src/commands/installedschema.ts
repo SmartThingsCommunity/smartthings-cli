b71bcc64cc09495312954cc1e07f5b58f83cada7
Ross Stenersen <rossiam@gmail.com>
2022-09-06 10:34:51 -0500

    refactor: move installedschema utilities into -util file

--- packages/cli/src/commands/installedschema.ts
+++ packages/cli/src/commands/installedschema.ts
@@ -1,36 +1,16 @@
 import { Flags } from '@oclif/core'
 
-import { InstalledSchemaApp, SmartThingsClient } from '@smartthings/core-sdk'
+import { InstalledSchemaApp } from '@smartthings/core-sdk'
 
-import { APICommand, OutputItemOrListConfig, outputItemOrList, TableFieldDefinition, withLocations } from '@smartthings/cli-lib'
+import { APICommand, OutputItemOrListConfig, outputItemOrList } from '@smartthings/cli-lib'
 
+import {
+	InstalledSchemaAppWithLocation,
+	installedSchemaInstances,
+	listTableFieldDefinitions,
+	tableFieldDefinitions,
+} from '../lib/commands/installedschema-util'
 
-export type InstalledSchemaAppWithLocation = InstalledSchemaApp & { location?: string }
-
-export const listTableFieldDefinitions = ['appName', 'partnerName', 'partnerSTConnection', 'isaId']
-export const tableFieldDefinitions: TableFieldDefinition<InstalledSchemaApp>[] = [
-	'appName', 'isaId', 'partnerName', 'partnerSTConnection', 'locationId',
-	'icon', 'icon2x', 'icon3x',
-]
-
-export async function installedSchemaInstances(client: SmartThingsClient, locationIds: string[], verbose: boolean): Promise<InstalledSchemaAppWithLocation[]> {
-	if (!locationIds) {
-		locationIds = (await client.locations.list()).map(it => it.locationId)
-	}
-
-	const installedApps = (await Promise.all(locationIds.map(async (locationId) => {
-		try {
-			return (await client.schema.installedApps(locationId))
-		} catch(e) {
-			return []
-		}
-	}))).flat()
-
-	if (verbose) {
-		return await withLocations(client, installedApps)
-	}
-	return installedApps
-}
 
 export default class InstalledSchemaAppsCommand extends APICommand<typeof InstalledSchemaAppsCommand.flags> {
 	static description = 'get a specific schema connector instance or a list of instances'
@@ -62,8 +42,10 @@ export default class InstalledSchemaAppsCommand extends APICommand<typeof Instal
 			listTableFieldDefinitions,
 			tableFieldDefinitions,
 		}
+
 		if (this.flags.verbose) {
 			config.listTableFieldDefinitions.splice(3, 0, 'location')
+			config.tableFieldDefinitions.splice(5, 0, 'location')
 		}
 
 		await outputItemOrList(this, config, this.args.id,
