d02317d71d181ea67c6229a94ccb7361c91856ec
Bob Florian <bob@smartthings.com>
2023-05-11 07:03:52 -0400

    chore: update AWS SDK

--- packages/cli/src/lib/aws-utils.ts
+++ packages/cli/src/lib/aws-utils.ts
@@ -1,8 +1,7 @@
-import AWS from 'aws-sdk'
-import { AddPermissionRequest, FunctionName, Principal, StatementId } from 'aws-sdk/clients/lambda'
+import { LambdaClient, AddPermissionCommand, AddPermissionRequest } from '@aws-sdk/client-lambda'
 
 
-export async function addPermission(arn: FunctionName, principal: Principal = '906037444270', statementId: StatementId = 'smartthings'): Promise<string> {
+export async function addPermission(arn: string, principal = '906037444270', statementId = 'smartthings'): Promise<string> {
 	const segs = arn.split(':')
 	if (segs.length < 7) {
 		return 'Invalid Lambda ARN'
@@ -10,8 +9,7 @@ export async function addPermission(arn: FunctionName, principal: Principal = '9
 
 	try {
 		const region = segs[3]
-		AWS.config.update({ region })
-		const lambda = new AWS.Lambda()
+		const client = new LambdaClient({ region })
 
 		const params: AddPermissionRequest = {
 			Action: 'lambda:InvokeFunction',
@@ -20,16 +18,19 @@ export async function addPermission(arn: FunctionName, principal: Principal = '9
 			StatementId: statementId,
 		}
 
-		await lambda.addPermission(params).promise()
+		const command = new AddPermissionCommand(params)
+
+		await client.send(command)
+
 		return 'Authorization added'
 	} catch (error) {
-		if (error.code === 'ResourceConflictException') {
+		if (error.name === 'ResourceConflictException') {
 			return 'Already authorized'
 		}
 		throw error
 	}
 }
 
-export function addSchemaPermission(arn: FunctionName, principal: Principal = '148790070172', statementId: StatementId = 'smartthings'): Promise<string> {
+export function addSchemaPermission(arn: string, principal = '148790070172', statementId = 'smartthings'): Promise<string> {
 	return addPermission(arn, principal, statementId)
 }
