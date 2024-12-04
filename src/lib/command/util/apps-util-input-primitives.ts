f8a019dec38414ed0852efc37065c621e5af9bbe
Ross Stenersen <rossiam@gmail.com>
2024-12-04 10:39:19 -0600

    refactor: convert apps:oauth:generate command to yargs

--- src/lib/command/util/apps-util-input-primitives.ts
+++ src/lib/command/util/apps-util-input-primitives.ts
@@ -24,6 +24,7 @@ export const oauthAppScopeDef = checkboxDef<string>('Scopes', availableScopes, {
 		'To determine which scopes you need for the application, see documentation for the' +
 		' individual endpoints you will use in your app:\n' +
 		'  https://developer.smartthings.com/docs/api/public/',
+	validate: (chosen: string[]) => chosen.length > 0 || 'At least one scope is required.',
 })
 
 const redirectUriHelpText = 'More information on redirect URIs can be found at:\n' +
