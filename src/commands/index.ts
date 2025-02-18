960b4e835d851521979c0adbab2c41cced15d818
Ross Stenersen <rossiam@gmail.com>
2025-02-18 15:05:04 -0600

    refactor: convert devicepreferences:create command to yargs

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -21,6 +21,7 @@ import capabilitiesTranslationsCommand from './capabilities/translations.js'
 import configCommand from './config.js'
 import configResetCommand from './config/reset.js'
 import devicepreferencesCommand from './devicepreferences.js'
+import devicepreferencesCreateCommand from './devicepreferences/create.js'
 import devicepreferencesTranslationsCommand from './devicepreferences/translations.js'
 import devicepreferencesTranslationsCreateCommand from './devicepreferences/translations/create.js'
 import deviceprofilesCommand from './deviceprofiles.js'
@@ -68,6 +69,7 @@ export const commands: CommandModule<object, any>[] = [
 	configCommand,
 	configResetCommand,
 	devicepreferencesCommand,
+	devicepreferencesCreateCommand,
 	devicepreferencesTranslationsCommand,
 	devicepreferencesTranslationsCreateCommand,
 	deviceprofilesCommand,
