e6ce8956d716efdbd4a0ddf72399ed136db40fc5
Ross Stenersen <rossiam@gmail.com>
2025-04-01 09:31:34 -0500

    refactor: convert virtualdevices:create command to yargs

---
+++ src/__tests__/lib/command/util/edge/drivers-choose.test.ts
@@ -0,0 +1,64 @@
+import { jest } from '@jest/globals'
+
+import type { DriverChoice } from '../../../../../lib/command/util/drivers-choose.js'
+import type { ChooseFunction, createChooseFn } from '../../../../../lib/command/util/util-util.js'
+import type { listDrivers } from '../../../../../lib/command/util/edge/drivers-util.js'
+import { EdgeDriver, SmartThingsClient } from '@smartthings/core-sdk'
+
+
+const createChooseFnMock = jest.fn<typeof createChooseFn<DriverChoice>>()
+jest.unstable_mockModule('../../../../../lib/command/util/util-util.js', () => ({
+	createChooseFn: createChooseFnMock,
+}))
+
+const listDriversMock = jest.fn<typeof listDrivers>()
+jest.unstable_mockModule('../../../../../lib/command/util/edge/drivers-util.js', () => ({
+	listDrivers: listDriversMock,
+}))
+
+
+const { chooseDriverFn } = await import('../../../../../lib/command/util/drivers-choose.js')
+
+
+describe('createDriverFn', () => {
+	const createDriverMock = jest.fn<ChooseFunction<DriverChoice>>()
+	createChooseFnMock.mockReturnValue(createDriverMock)
+
+	const drivers = [] as EdgeDriver[]
+
+	const client = { drivers: {} } as SmartThingsClient
+
+	it('does not include all organizations by default', async () => {
+		expect(chooseDriverFn()).toBe(createDriverMock)
+
+		expect(createChooseFnMock).toHaveBeenCalledExactlyOnceWith(
+			expect.objectContaining({ itemName: 'driver' }),
+			expect.any(Function),
+		)
+
+		const listItems = createChooseFnMock.mock.calls[0][1]
+
+		listDriversMock.mockResolvedValueOnce(drivers)
+
+		expect(await listItems(client)).toBe(drivers)
+
+		expect(listDriversMock).toHaveBeenCalledExactlyOnceWith(client, undefined)
+	})
+
+	it('requests all organizations when specified', async () => {
+		expect(chooseDriverFn({ includeAllOrganizations: true })).toBe(createDriverMock)
+
+		expect(createChooseFnMock).toHaveBeenCalledExactlyOnceWith(
+			expect.objectContaining({ itemName: 'driver' }),
+			expect.any(Function),
+		)
+
+		const listItems = createChooseFnMock.mock.calls[0][1]
+
+		listDriversMock.mockResolvedValueOnce(drivers)
+
+		expect(await listItems(client)).toBe(drivers)
+
+		expect(listDriversMock).toHaveBeenCalledExactlyOnceWith(client, true)
+	})
+})
